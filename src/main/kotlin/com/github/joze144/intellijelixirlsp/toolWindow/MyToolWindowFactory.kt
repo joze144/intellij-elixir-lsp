f07cf60b8e53e76054db2d7a2d43c0af61a16b98
GitHub Action <action@github.com>
2024-06-27 06:22:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/joze144/intellijelixirlsp/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/joze144/intellijelixirlsp/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.joze144.intellijelixirlsp.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.joze144.intellijelixirlsp.MyBundle
+import com.github.joze144.intellijelixirlsp.services.MyProjectService
 import javax.swing.JButton
 
 
